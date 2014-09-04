25ba1a1b452e010a80716a001e7c24b188b2a958
taoyuan <torworx@gmail.com>
2014-09-04 15:24:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+#  [![Build Status](https://img.shields.io/travis/taoyuan/sira-creds.svg?style=flat)](http://travis-ci.org/taoyuan/sira-creds)
+
+> Sira credentials module to support third party account linking
+
+## Installation
+
+```bash
+$ npm install sira-creds
+```
+
+## Documentation
+
+_(Coming soon)_
+
+## Examples
+
+_(Coming soon)_
+
+## License
+
+Copyright (c) 2014 Tao Yuan Licensed under the MIT license.
---
+++ test/support.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var chai = require('chai');
+chai.config.includeStack = true;
+
+exports.t = exports.assert = chai.assert;
+
+exports.donner = function donner(n, func) {
+    if (n < 1) {
+        return func();
+    }
+    return function (err) {
+        if (err) throw err;
+        if (--n < 1) {
+            func(err ? err : null);
+        }
+    };
+};
+
+exports.sapp = function (options) {
+    options = options || {};
+    options.db = options.db || {
+        driver: 'redis-hq'
+    };
+
+    var sapp = new sira.Application;
+    sapp.setAll(options);
+    sapp.phase(sira.boot.module('./'));
+    sapp.phase(sira.boot.database(options.db));
+    sapp.phase(authorizer);
+    return sapp;
+};
+
+exports.cleanup = function (sappOrModels, done) {
+    var models = sappOrModels;
+    if (sappOrModels.models) {
+        models = _.values(sappOrModels.models);
+    } else if (!(Array.isArray(sappOrModels))) {
+        models = [sappOrModels];
+    }
+
+    done = done || function () {};
+
+    async.eachSeries(models, function (Model, callback) {
+        Model.destroyAll(callback);
+    }, done);
+};
